df66dfcfa21e7d2cd40eb6c948ec87076a561ee8
cal1801 <pike1801@gmail.com>
2017-09-06 21:45:33 -0400

    email updates

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,25 +77,10 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  #devise
-  config.action_mailer.default_url_options = { host: 'book-a-retreat.herokuapp.com' }
-  config.action_mailer.asset_host = "http://book-a-retreat.herokuapp.com"
+  # devise
+  config.action_mailer.default_url_options = { host: 'www.bookaretreat.com' }
+  config.action_mailer.asset_host = "http://www.bookaretreat.com"
   config.assets.initialize_on_precompile = false
-
-  #config.action_mailer.delivery_method = :smtp
-  #config.action_mailer.perform_deliveries = true
-  #config.action_mailer.raise_delivery_errors = false
-  #config.action_mailer.default :charset => "utf-8"
-
-  #config.action_mailer.smtp_settings = {
-  #   :tls => true,
-  #   :address => "smtp.gmail.com",
-  #   :port => 587,
-  #   :domain => "gmail.com",
-  #   :authentication => :login,
-  #   :user_name => "[username]",
-  #   :password => "[password]"
-  # }
 
   #S3 server settings
   config.paperclip_defaults = {
